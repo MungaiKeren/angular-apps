57b6562c6d51c74dcd16347761b104f691703cb1
MungaiKeren <wambukeren@gmail.com>
2019-08-21 11:19:16 +0300

    did an environment class that should helpus add the api key

---
+++ src/app/environments.ts
@@ -0,0 +1,5 @@
+export const Environments = {
+    production: false,
+    apiUrl:"http://quotes.stormconsultancy.co.uk/random.json"
+    //created a property apiUrl and assigned it to our random quotes API url
+}
